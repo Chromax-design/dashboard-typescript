ae64c5821ba09b925bc2a08c01082997b8235fab
chromax-design <chromax.design@gmail.com>
2025-04-15 15:42:36 -0700

    replace img with Image

--- components/UnsplashComponent.tsx
+++ components/UnsplashComponent.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { useGetrandomImageQuery } from "@/services/unsplash"
+import Image from "next/image"
 
 const UnsplashComponent = () => {
-    const { data, isLoading } = useGetrandomImageQuery()
+    const { data } = useGetrandomImageQuery()
     return (
-        <div className="col-span-12 sm:col-span-6 lg:col-span-8 sticky top-0 h-screen">
-            {isLoading ? <div className="w-full h-full bg-gray-100" /> : <img src={data} alt="Just portraits" className=" w-full object-cover object-top h-full transition-opacity duration-300 ease-in-out" />}
+        <div className="col-span-12 sm:col-span-6 lg:col-span-8 sticky top-0 h-screen bg-gray-100">
+            {data && <Image src={data} alt="Just portraits" className=" w-full object-cover object-top h-full transition-opacity duration-300 ease-in-out" width={500} height={700} priority />}
         </div>
     )
 }
