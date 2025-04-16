55759dc8ac564ab1571c1609f64ec9947bac1d50
chromax-design <chromax.design@gmail.com>
2025-04-16 12:39:43 -0700

    edited unsplash api

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -8,7 +8,7 @@ const TopBar = () => {
   const { open, isMobile } = useSidebar()
 
   return (
-    <div className={`fixed top-0 right-0 ${open ? 'w-[calc(100%-16rem)] duration-300' : 'w-full'} transition-all ease-in-out bg-white h-14 p-3 ${isMobile ? 'w-full' : ''}`}>
+    <div className={`fixed top-0 right-0 max-md:w-full ${open ? 'w-[calc(100%-16rem)] duration-300' : 'w-full'} transition-all ease-in-out bg-white h-14 p-3 ${isMobile ? 'w-full shadow' : ''}`}>
       <AppSidebar />
       <div className="flex justify-between items-center">
         <SidebarTrigger />
--- components/UnsplashComponent.tsx
+++ components/UnsplashComponent.tsx
@@ -7,7 +7,7 @@ const UnsplashComponent = () => {
     const { data } = useGetrandomImageQuery()
     return (
         <div className="col-span-12 sm:col-span-6 lg:col-span-8 sticky top-0 h-screen bg-gray-100">
-            {data && <Image src={data} alt="Just portraits" className=" w-full object-cover object-top h-full transition-opacity duration-300 ease-in-out" width={500} height={700} priority />}
+            {data && <Image src={data} alt="Just portraits" className=" w-full object-cover object-center h-full transition-opacity duration-300 ease-in-out" width={500} height={700} priority />}
         </div>
     )
 }
