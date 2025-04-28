af1d76ac495828dedb12065ecdd9ee30497f4aeb
chromax-design <chromax.design@gmail.com>
2025-04-28 10:29:20 -0700

    corrected the UI mistake

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
     redirect("/");
   }
   return (
-    <main className="sm:grid sm:grid-cols-12 w-full gap-0 max-sm:p-20 max-sm:min-h-screen max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center">
+    <main className="sm:grid sm:grid-cols-12 w-full gap-0 max-sm:py-20 max-sm:min-h-screen max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center">
       <div className="max-sm:h-full sm:col-span-6 lg:col-span-4 flex justify-center flex-col items-center w-full p-6 md:p-10 space-y-4">
         {children}
       </div>
